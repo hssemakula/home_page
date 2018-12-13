ec68538b8020f3597dcbe9774331a6ee59eec4df
hssemakula <hillary_ssemakula@student.uml.edu>
2018-12-13 15:49:49 -0500

    BLANK TILE

--- assignment9/js/index.js
+++ assignment9/js/index.js
@@ -8,6 +8,7 @@ File: https://hssemakula.github.io/home_page/assignment8/js/index.js
   of the Row Scrabble defined by the file https://hssemakula.github.io/home_page/assignment9/index.html.
 */
 var json; //object to store json object that will be extracted from data.json
+var dictionary;
 var tilesRemaining = 100; //variable to keep count of the tiles remaining
 var currentWordArray = ["", "", "", "", "", "", ""]; //array of single character Strings: keeps track of the word spelled on the scrabble board
 var isVacant = [true, true, true, true, true, true, true]; //array of boolean values:keeps track of which slot the scrabble board has a tile or not.
@@ -17,6 +18,11 @@ var totalScore = 0; //keeps track of the total score
 //getJSON method, used here because XMLHttpRequest() was denied access to the json file through https://hssemakula.github.io/home_page/assignment9/data/data.json
 $.getJSON("./data/data.json", function(userData) {
   json = userData;
+});
+
+$.getJSON("./data/dict.json", function(userData) {
+  dictionary = userData;
+  console.log(dictionary[0]);
 });
 
 //function is run when the page has loaded.
@@ -136,45 +142,6 @@ function attachDraggableAbility() {
     containment: "#body-div" //keep draggable from scrolling off screen.
   }); //make all images with class "draggable", draggable.
 }
-/* This function gets 7 tiles from the tile rack. */
-/*function getTiles(json_data) {
-  $(".draggable").data("onSlot", -1); //initialize all tiles to indicate that they are not on the board at any slot(-1)
-
-  var tilesDrawn = 0; //number of tiles currently drawn from rack
-  var currentImgToReplace = 1; //id number of image to replace on page
-  var dataSize = parseInt(json_data.length); //number of elements in json object
-
-  /* This is a while loop that keeps relacing the src of the img tag of an image on the page whose
-  ID is  "currentImgToReplace" with the src field of a randomly chosen object in the json object passed.
-  This loop has only terminates if 7 src attributes have been cahnged(7 tiles drawn) or if there are no
-  more tiles(tilesRemaining == 0). OR if both condition sfail.
-
-  */
-/*  while (tilesDrawn < 7 && tilesRemaining > 0) {
-
-    //This line of code returns a random number between 0 and 1 - size of the json object passed.
-    //this acts as a random index to choose a random letter and is regenerated on every loop iteration.
-    var randomIndex = Math.floor(Math.random() * dataSize);
-
-    /*This If statement takes care of updating values. i.e it updates the number of tiles drawn,
-    the number of times Remaining, the amount of a certain letter left and the next IMG tag to be cahnged.
-    it is only executed if the current letter chosen randomly doesn't have an amount of 0 and the current IMG tag to be replaced
-    has not already been replaced
-    */
-/*  if (parseInt(json_data[randomIndex].amount) > 0 && $("#" + currentImgToReplace).attr('src') === "") {
-      $("#" + currentImgToReplace).attr('src', json_data[randomIndex].src);
-      json_data[randomIndex].amount = parseInt(json_data[randomIndex].amount) - 1;
-      tilesDrawn = tilesDrawn + 1;
-      tilesRemaining = tilesRemaining - 1;
-      currentImgToReplace = currentImgToReplace + 1;
-      $("#tileCount").html(tilesRemaining); //update label showing number of tiles remaining
-    } else if (!($("#" + currentImgToReplace).attr('src') === "")) {
-      tilesDrawn = tilesDrawn + 1; //if src attribute is not empty then count a tile(i.e the rack has a tile)
-      currentImgToReplace = currentImgToReplace + 1;
-    }
-  }
-
-} */
 
 /* This function gets 7 tiles from the tile rack. */
 function getTiles(json_data) {
@@ -184,12 +151,6 @@ function getTiles(json_data) {
   var currentImgToReplace = 1; //id number of image to replace on page
   var dataSize = parseInt(json_data.length); //number of elements in json object
 
-  /* This is a while loop that keeps relacing the src of the img tag of an image on the page whose
-  ID is  "currentImgToReplace" with the src field of a randomly chosen object in the json object passed.
-  This loop has only terminates if 7 src attributes have been cahnged(7 tiles drawn) or if there are no
-  more tiles(tilesRemaining == 0). OR if both condition sfail.
-
-  */
   for (currentImgToReplace = 1; currentImgToReplace <= 7; currentImgToReplace++) {
     if (tilesRemaining == 0) {
       //show popup
@@ -208,10 +169,6 @@ function getTiles(json_data) {
       $("#tileCount").html(tilesRemaining); //update label showing number of tiles remaining
 
     }
-
-
-
-
   }
 }
 
